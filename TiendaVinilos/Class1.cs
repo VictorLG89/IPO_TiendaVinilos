9e12b2f3b9af2cb9364504c704bd1ddce73c0e03
VictorLG89 <victor.lay@alu.uclm.es>
2024-02-07 10:14:47 +0100

    Lista de deseos

--- TiendaVinilos/Class1.cs
+++ TiendaVinilos/Class1.cs
@@ -6,7 +6,59 @@ using System.Threading.Tasks;
 
 namespace TiendaVinilos
 {
-    internal class Class1
+    using System.ComponentModel;
+
+    public class Vinilo : INotifyPropertyChanged
     {
+        private string _nombre;
+        private string _autor;
+        private string _portada;
+
+        public string Nombre
+        {
+            get { return _nombre; }
+            set
+            {
+                if (_nombre != value)
+                {
+                    _nombre = value;
+                    OnPropertyChanged(nameof(Nombre));
+                }
+            }
+        }
+
+        public string Autor
+        {
+            get { return _autor; }
+            set
+            {
+                if (_autor != value)
+                {
+                    _autor = value;
+                    OnPropertyChanged(nameof(Autor));
+                }
+            }
+        }
+
+        public string Portada
+        {
+            get { return _portada; }
+            set
+            {
+                if (_portada != value)
+                {
+                    _portada = value;
+                    OnPropertyChanged(nameof(Portada));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
+
 }
