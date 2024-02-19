399c909faebf41ac8f676cc0d290fdf2c6731e8d
VictorLG89 <victor.lay@alu.uclm.es>
2024-02-19 08:43:41 +0100

    .

--- TiendaVinilos/obj/Debug/TiendaVinilos_Content.g.i.cs
+++ TiendaVinilos/obj/Debug/TiendaVinilos_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/disco-de-vinilo.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("disco-de-vinilo.png")]
 
 
