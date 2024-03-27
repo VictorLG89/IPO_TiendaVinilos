61b6ebacdfccde9b56cfafe4832d4e49afa3da70
VictorLG89 <victor.lay@alu.uclm.es>
2024-03-27 14:19:22 +0100

    main window de admin hecha

---
+++ TiendaVinilos/PromocionesAdmin.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TiendaVinilos
+{
+    /// <summary>
+    /// Lógica de interacción para PromocionesAdmin.xaml
+    /// </summary>
+    public partial class PromocionesAdmin : Window
+    {
+        public PromocionesAdmin()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
