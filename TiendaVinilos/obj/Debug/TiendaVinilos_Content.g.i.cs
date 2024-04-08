87e27f50ba4c4590ff555896bba11b26ae4e5ab1
victorialpzzz <victorialopez26@icloud.com>
2024-04-08 20:10:52 +0100

    5

--- TiendaVinilos/obj/Debug/TiendaVinilos_Content.g.i.cs
+++ TiendaVinilos/obj/Debug/TiendaVinilos_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("logotienda.jpg")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/corazonvacio.png")]
 
 
